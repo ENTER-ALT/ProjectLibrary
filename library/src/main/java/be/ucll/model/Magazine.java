e564ee75aff07f6e4d030bb53710123cfc5e9de6
Rostislav-Dusnii <r0979933@ucll.be>
2024-03-16 17:51:37 +0100

    04 Alter magazine

--- library/src/main/java/be/ucll/model/Magazine.java
+++ library/src/main/java/be/ucll/model/Magazine.java
@@ -1,29 +1,17 @@
 package be.ucll.model;
 
-import be.ucll.utilits.TimeTracker;
-
-public class Magazine {
+public class Magazine extends Publication {
     
-    private String title;
     private String editor;
     private String ISSN;
-    private Integer year;
 
-    public static final String INVALID_TITLE_EXCEPTION = "Title is required";
     public static final String INVALID_EDITOR_EXCEPTION = "Editor is required";
     public static final String INVALID_ISSN_EXCEPTION = "ISSN is required";
-    public static final String NONPOSITIVE_YEAR_EXCEPTION = "Publication year must be a positive number";
-    public static final String FUTURE_YEAR_EXCEPTION = "Publication year cannot be in the future";
     
-    public Magazine(String title, String editor, String ISSN, Integer year) {
-        setTitle(title);
+    public Magazine(String title, String editor, String ISSN, Integer year, Integer availableCopies) {
+        super(title, year, availableCopies);
         setEditor(editor);
         setISSN(ISSN);
-        setYear(year);
-    }
-
-    public String getTitle() {
-        return title;
     }
 
     public String getEditor() {
@@ -32,18 +20,6 @@ public class Magazine {
 
     public String getISSN() {
         return ISSN;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setTitle(String title) {
-        if (title != null && !title.isBlank()) {
-            this.title = title;
-        } else {
-            throw new DomainException(INVALID_TITLE_EXCEPTION);
-        }
     }
 
     public void setEditor(String editor) {
@@ -60,18 +36,4 @@ public class Magazine {
         } 
         this.ISSN = ISSN;
     }
-
-    public void setYear(Integer year) {
-        if (year != null && year > 0) {
-            int currentYear = TimeTracker.GetCurrentYear();
-            if (year <= currentYear) {
-                this.year = year;
-            } else {
-                throw new DomainException(FUTURE_YEAR_EXCEPTION);
-            }
-        } else {
-            throw new DomainException(NONPOSITIVE_YEAR_EXCEPTION);
-        }
-    }
-
 }
