83b896b867006951b23eb34f487b981dc61657f9
Rostislav-Dusnii <r0979933@ucll.be>
2024-03-18 22:13:15 +0100

    06- add UserController, UserRepository, UserService, UserServiceTest

---
+++ library/src/main/java/be/ucll/controller/UserRestController.java
@@ -0,0 +1,30 @@
+package be.ucll.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import be.ucll.model.User;
+import be.ucll.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/users")
+public class UserRestController {
+    
+    private UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    
+}
---
+++ library/src/test/java/be/ucll/service/UserServiceTest.java
@@ -0,0 +1,59 @@
+package be.ucll.service;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import be.ucll.model.User;
+import be.ucll.repository.UserRepository;
+
+public class UserServiceTest {
+
+    @Test
+    public void givenValidRequest_whenGettingAllUsers_thanTheCertainUsersReturned() {
+        List<User> expectedUsers = createDefaultUserList();
+        UserRepository repository = createDefaultRepository(expectedUsers);
+        UserService service = createDefaultService(repository);
+
+        List<User> actualUsers = service.getAllUsers();
+
+        assertEquals(expectedUsers.size(), actualUsers.size());
+        assertArrayEquals(expectedUsers.toArray(), actualUsers.toArray());
+    }
+
+    @Test
+    public void givenNullUsers_whenGettingAllUsers_thanNullUsersReturned() {
+        List<User> expectedUsers = null;
+        UserRepository repository = createDefaultRepository(expectedUsers);
+        UserService service = createDefaultService(repository);
+
+        List<User> actualUsers = service.getAllUsers();
+
+        assertEquals(expectedUsers, actualUsers);
+    }
+
+    public UserRepository createDefaultRepository(List<User> users) {
+        return new UserRepository(users);
+    }
+
+    public UserService createDefaultService(UserRepository repository) {
+        return new UserService(repository);
+    }
+
+    public UserService createDefaultService() {
+        return new UserService(createDefaultRepository(createDefaultUserList()));
+    }
+
+    public List<User> createDefaultUserList() {
+        return List.of(
+            new User("John Doe", 25, "john.doe@ucll.be", "john1234"),
+            new User("Jane Toe", 30, "jane.toe@ucll.be", "jane1234"),
+            new User("Jack Doe", 5, "jack.doe@ucll.be", "jack1234"),
+            new User("Sarah Doe", 4, "sarah.doe@ucll.be", "sarah1234"),
+            new User("Birgit Doe", 18, "birgit.doe@ucll.be", "birgit1234")
+            );
+    }
+}
