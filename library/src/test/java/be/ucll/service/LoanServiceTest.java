48bcdb5a3c6b6cf4fa0e51f3f467941f112c9416
Rostislav-Dusnii <r0979933@ucll.be>
2024-03-22 10:56:11 +0100

    small fix

--- library/src/test/java/be/ucll/service/LoanServiceTest.java
+++ library/src/test/java/be/ucll/service/LoanServiceTest.java
@@ -1,6 +1,5 @@
 package be.ucll.service;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
