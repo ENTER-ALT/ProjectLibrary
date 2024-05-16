a23b7262cc1e3611853c382bc94af323606e6909
Rostislav-Dusnii <r0979933@ucll.be>
2024-05-16 15:38:26 +0200

    refactor

--- library/src/test/java/be/ucll/service/UserServiceTest.java
+++ library/src/test/java/be/ucll/service/UserServiceTest.java
@@ -3,7 +3,6 @@ package be.ucll.service;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
